d3b5baf5c90f17de0db910f33068c81ea6e4c734
Jeganathan Swaminathan <mail2jegan@gmail.com>
2023-11-20 18:02:47 +0530

    Added Day1 lab exercises.

---
+++ Day1/hello-cpp/source/hello.h
@@ -0,0 +1,8 @@
+#include <iostream>
+
+class Hello {
+public:
+	Hello();
+	~Hello();
+	std::string sayHello();
+};
---
+++ Day1/hello-cpp/source/main.cpp
@@ -0,0 +1,9 @@
+#include "hello.h"
+
+int main() {
+
+	Hello hello;
+	std::cout << hello.sayHello() << std::endl;
+
+	return 0;
+}
