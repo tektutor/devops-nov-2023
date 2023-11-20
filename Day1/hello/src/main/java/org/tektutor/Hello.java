4d329054473aa5b6f15ce2f17f4301f530331c8e
Jeganathan Swaminathan <mail2jegan@gmail.com>
2023-11-20 14:53:19 +0530

    Added Day1 lab exercises.

---
+++ Day1/hello/src/main/java/org/tektutor/Hello.java
@@ -0,0 +1,16 @@
+package org.tektutor;
+
+public class Hello {
+
+	public String sayHello() {
+		return "Hello World!";
+	}
+
+	public static void main ( String[] args ) {
+
+		Hello hello = new Hello();
+		System.out.println ( hello.sayHello() );
+
+	}
+
+}
